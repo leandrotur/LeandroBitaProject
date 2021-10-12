fd0d89e07e94a0c72aa57893bed5bf56c76e315a
leandrotur <leandroturdera1982@gmail.com>
2021-10-12 15:00:30 -0300

    API CALL Example: http://127.0.0.1:5000/market_data/statistics?ticker=IBM&fromdate=2021-10-10
    MVP delivered:
    Features:
    Crear una API usando Flask. Esta API tendrá un solo endpoint. El endpoint deberá calcular estadísticos para la acción que requiera el usuario.
    La data de las acciones se puede obtener de la API de AlphaVantage. La documentación está aquí: https://www.alphavantage.co/documentation/
    
    El endpoint recibirá los siguientes parámetros.
    
    ticker: identificador de la acción. (ejemplo: "IBM").
    
    start_date: día de inicio para el cálculo de los estadísticos.
    
    Los estadísticos que hay que calcular son:
    
    Return.
    Annualized Return.
    Standard Deviation.
    Annualized Standard Deviation.
    Sharpe Ratio.
    
    El endpoint debe traer la siguiente data:
    
    Ticker.
    Todos los estadísticos calculados.
    El último precio disponible para esa acción.

--- app.py
+++ app.py
@@ -3,15 +3,6 @@ from market_data.routes import market_data_bp
 
 app = Flask(__name__)
 
-home = Blueprint('home', __name__, static_folder='static', template_folder='templates')
-
-
-@home.route('/')
-def index():
-    return "This is the index route."
-
-
-app.register_blueprint(home)
 
 app.register_blueprint(market_data_bp, url_prefix='/market_data')
 
